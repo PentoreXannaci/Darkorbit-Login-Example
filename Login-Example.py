5a466520934511303abf9b580e9d66d5a882b778
PNTX <bolter.daniel@hotmail.com>
2021-09-11 11:55:15 +0200

    added do-client useragent

--- Login-Example.py
+++ Login-Example.py
@@ -1,11 +1,18 @@
 #       Darkorbit Login Example
 #       https://github.com/PentoreXannaci
 
+import xml.etree.ElementTree as ET
 import requests
 from bs4 import BeautifulSoup
 
-
+darkorbit_client_updates_url = "https://darkorbit-22-client.bpsecure.com/bpflashclient/windows.x64/repository/Updates.xml"
 darkorbit_url = "https://www.darkorbit.com"
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br"
+    }
 
 
 def login(username, password):
@@ -21,7 +28,7 @@ def login(username, password):
     login_data = {"username": username, "password": password}
 
     #send the login post request with the post data from above.
-    login_response = requests.post(login_url, login_data)
+    login_response = requests.post(login_url, login_data, headers = headers)
     
     #check if the login worked. (search a specific string in the html content which is only present when logged in.)
     if "header_uri" in str(login_response.content):
@@ -32,7 +39,7 @@ def login(username, password):
 
 def get_login_url():
     #before we can send the login post request. we need to fetch the login url out of the darkorbit site.
-    page_response = requests.get(darkorbit_url)
+    page_response = requests.get(darkorbit_url, headers = headers)
 
     #init BeautifulSoup with the html response. (pip install beautifulsoup4)
     index_soup = BeautifulSoup(page_response.content, features="html.parser")
@@ -46,6 +53,16 @@ def get_login_url():
 
     return login_url
 
+def set_useragent():
+    updates_response = requests.get(darkorbit_client_updates_url, headers = headers)
+    tree = ET.fromstring(updates_response.content)
+
+    package_update_version = tree.find(".//Version").text
+    Pheaders["User-Agent"] = f"BigPointClient/{package_update_version}"
+
+
+#set the useragent to the darkorbit client useragent. 
+set_useragent()
 
 username = input("Username: ")
 password = input("Password: ")
